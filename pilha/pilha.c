38ab91992f709206bb54ea78de76af2b7d413d2d
Victor Moreno <victorgalves@hotmail.com>
2019-04-02 11:15:48 -0300

    Criação das funções init, isempty, isfull e push

---
+++ pilha/pilha.c
@@ -0,0 +1,25 @@
+#include "pilha.h"
+#include <stdio.h>
+
+void stack_init(pilha * p) {
+    p->topo = -1;
+}
+
+bool stack_isempty(pilha p) {
+    if(p.topo < 0) 
+        return true;
+    return false;
+}
+
+bool stack_isfull(pilha p) {
+    if(p.topo == STACK_MAX - 1) 
+        return true;
+    return false;
+}
+
+void push(pilha * p, stack_info x) {
+    if(!stack_isfull(*p)) {
+        p->topo++;
+        p->itens[p->topo] = x;
+    }
+}
